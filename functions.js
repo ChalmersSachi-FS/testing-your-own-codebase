a89ea201176d27cfdbe746ed2aa06fc6a8fec996
Sachi Chalmers <sachichalmers@Mac.lan>
2025-01-31 17:38:45 -0500

    Updated functions.js
    Found Error in code and fixed it.

--- functions.js
+++ functions.js
@@ -10,7 +10,8 @@ function sumOfArray(arr) {
 // Reverse String
 
 function reverseString(str) {
-  if (typeof str !== "string") throw new Error("input must be a string");
+  if (typeof str !== "string") throw new Error("Input must be a string");
+  return str.split("").reverse().join("");
 }
 
 // Check Palindrome
