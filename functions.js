faa38fb630dedd67fa2c285f460444a18d737507
Sachi Chalmers <sachichalmers@Mac.lan>
2025-01-31 16:32:15 -0500

    functions.js
    Created functions.js file & added code into file

---
+++ functions.js
@@ -0,0 +1,31 @@
+// Four Functions
+
+// Sum of Array
+
+function sumOfArray(arr) {
+  if (!Array.isArray(arr)) throw new Error("Input must be an array");
+  return arr.reduce((sum, num) => sum + num, 0);
+}
+
+// Reverse String
+
+function reverseString(str) {
+  if (typeof str !== "string") throw new Error("input must be a string");
+}
+
+// Check Palindrome
+function isPalindrome(str) {
+  if (typeof str !== "string") throw new Error("Input must be a string");
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+
+// Find Max in Array
+function findMax(arr) {
+  if (!Array.isArray(arr) || arr.length === 0)
+    throw new Error("Input must be a non-empty array");
+  return Math.max(...arr);
+}
+
+// Export functions for testing
+module.exports = { sumOfArray, reverseString, isPalindrome, findMax };
